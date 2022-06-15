85509fbf52242c29eca373047110fbb518361942
BUMBAIYA <amitchauhan263871@gmail.com>
2022-06-15 20:59:33 +0530

    pass: view Matrix and camera movement

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -3,6 +3,7 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
 import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
@@ -16,42 +17,105 @@ public class MainGameLoop {
 
 	public static void main(String[] args) {
 		
+		float[] vertices = {			
+				-0.5f,0.5f,0,	
+				-0.5f,-0.5f,0,	
+				0.5f,-0.5f,0,	
+				0.5f,0.5f,0,		
+				
+				-0.5f,0.5f,1,	
+				-0.5f,-0.5f,1,	
+				0.5f,-0.5f,1,	
+				0.5f,0.5f,1,
+				
+				0.5f,0.5f,0,	
+				0.5f,-0.5f,0,	
+				0.5f,-0.5f,1,	
+				0.5f,0.5f,1,
+				
+				-0.5f,0.5f,0,	
+				-0.5f,-0.5f,0,	
+				-0.5f,-0.5f,1,	
+				-0.5f,0.5f,1,
+				
+				-0.5f,0.5f,1,
+				-0.5f,0.5f,0,
+				0.5f,0.5f,0,
+				0.5f,0.5f,1,
+				
+				-0.5f,-0.5f,1,
+				-0.5f,-0.5f,0,
+				0.5f,-0.5f,0,
+				0.5f,-0.5f,1
+				
+		};
+		
+		float[] textureCoordinates = {
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
+		};
+		
 		DisplayManager.createDisplay();
 		
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
 		Loader loader = new Loader();
 		
-		
-		float[] vertices = {
-				-0.5f, 0.5f, 0,
-				-0.5f, -0.5f, 0,
-				0.5f, -0.5f, 0,
-				0.5f, 0.5f, 0
-		};
-		
-		int[] indices = {
-			0, 1, 3,
-			3, 1, 2
-		};
-		
-		float[] textureCoordinates = {
-				0, 0,
-				0, 1,
-				1, 1,
-				1, 0
-		};
-		
 		RawModel rawModel = loader.loadToVAO(vertices, indices, textureCoordinates);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
 		TexturedModel texturedModel = new TexturedModel(rawModel, texture);
 		
-		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -1), 0, 0, 0, 0.5f);
+		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+		
+		Camera camera = new Camera();
 		
 		while (!Display.isCloseRequested()) {
-			entity.increasePosition(0, 0, 0.001f);
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
 			renderer.prepare();
 			shader.start();
+			shader.loadViewMatrix(camera);
 			renderer.render(entity, shader);
 			shader.stop();
 			
