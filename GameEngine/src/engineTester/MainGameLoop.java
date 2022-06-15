148d02a43ece000ee92e5dc31e33c028f23d5113
BUMBAIYA <amitchauhan263871@gmail.com>
2022-06-15 18:23:29 +0530

    Pass: shader vertex and fragment

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -6,6 +6,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
@@ -16,24 +17,33 @@ public class MainGameLoop {
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
 		
+		StaticShader shader = new StaticShader();
+		
 		float[] vertices = {
 				-0.5f, 0.5f, 0f,
 				-0.5f, -0.5f, 0f,
 				0.5f, -0.5f, 0f,
-				0.5f, -0.5f, 0f,
-				0.5f, 0.5f, 0f,
-				-0.5f, 0.5f, 0f
+				0.5f, 0.5f, 0f
 		};
 		
-		RawModel model = loader.loadToVAO(vertices);
+		int[] indices = {
+			0, 1, 3,
+			3, 1, 2
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, indices);
 		
 		while (!Display.isCloseRequested()) {
 			
 			renderer.prepare();
+			shader.start();
 			renderer.render(model);
+			shader.stop();
+			
 			DisplayManager.updateDisplay();
 		}
 		
+		shader.cleanUp();
 		loader.cleanUp();
 		
 		DisplayManager.closeDisplay();
