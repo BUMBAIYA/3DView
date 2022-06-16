97c7ce191f2d4c6c93b50d089edcbacfbb14ddcd
BUMBAIYA <amitchauhan263871@gmail.com>
2022-06-16 22:42:41 +0530

    removed: 500 cube demo.

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -1,9 +1,5 @@
 package engineTester;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
@@ -25,8 +21,8 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
 		
-		RawModel rawModel = OBJLoader.loadObjModel("rainbowCube", loader);
-		TexturedModel texturedModel = new TexturedModel(rawModel, new ModelTexture(loader.loadTexture("rainbow")));
+		RawModel rawModel = OBJLoader.loadObjModel("dragon", loader);
+		TexturedModel texturedModel = new TexturedModel(rawModel, new ModelTexture(loader.loadTexture("darkGreen")));
 		
 		ModelTexture texture = texturedModel.getTexture();
 		texture.setShineDamper(10);
@@ -39,23 +35,11 @@ public class MainGameLoop {
 		
 		MasterRenderer renderer = new MasterRenderer();
 		
-		List<Entity> allCubes = new ArrayList<Entity>();
-		Random random = new Random();
-		
-		for (int i = 0; i < 500; i++) {
-			float x = random.nextFloat() * 100 - 50;
-			float y = random.nextFloat() * 100 - 50;
-			float z = random.nextFloat() * 100 - 50;
-			allCubes.add(new Entity(texturedModel, new Vector3f(x, y, z), random.nextFloat() * 180f, random.nextFloat() * 180f, 0f, 1f));
-		}
-		
 		while (!Display.isCloseRequested()) {
 			entity.increaseRotation(0, 0.15f, 0);
 			camera.move();
 		
-			for (Entity cube: allCubes) {
-				renderer.processEntity(cube);
-			}
+			renderer.processEntity(entity);
 			
 			renderer.render(light, camera);
 			DisplayManager.updateDisplay();
