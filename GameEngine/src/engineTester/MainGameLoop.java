6b41a592f0e3471f464416920657384fa56788a6
BUMBAIYA <amitchauhan263871@gmail.com>
2022-06-22 14:49:06 +0530

    pass: lighting attenuation imp removed grass and tree entities

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -38,48 +38,22 @@ public class MainGameLoop {
 		
 		TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture, bTexture);
 		
-		ModelData flowerData = OBJFileLoader.loadOBJ("singleFlowerobj");
-		RawModel flowerModel = loader.loadToVAO(flowerData.getVertices(), flowerData.getTextureCoords(), flowerData.getNormals(), flowerData.getIndices());
+		ModelData lampData = OBJFileLoader.loadOBJ("lamp");
+		RawModel lampModel = loader.loadToVAO(lampData.getVertices(), lampData.getTextureCoords(), lampData.getNormals(), lampData.getIndices());
 		
-		ModelData grassData = OBJFileLoader.loadOBJ("grassModel");
-		RawModel grassModel = loader.loadToVAO(grassData.getVertices(), grassData.getTextureCoords(), grassData.getNormals(), grassData.getIndices());
-		
-		ModelData fernData = OBJFileLoader.loadOBJ("fern");
-		RawModel fernModel = loader.loadToVAO(fernData.getVertices(), fernData.getTextureCoords(), fernData.getNormals(), fernData.getIndices());
-		
-		TexturedModel flower = new TexturedModel(flowerModel, new ModelTexture(loader.loadTexture("flower")));
-		TexturedModel grass = new TexturedModel(grassModel, new ModelTexture(loader.loadTexture("grassTexture")));
-		ModelTexture fernTexture = new ModelTexture(loader.loadTexture("fernTextureAtlas"));
-		fernTexture.setNumberOfRows(2);
-		TexturedModel fern = new TexturedModel(fernModel, fernTexture);
-		flower.getTexture().setHasTransparency(true);
-		flower.getTexture().setUseFakeLighting(true);
-		grass.getTexture().setHasTransparency(true);
-		grass.getTexture().setUseFakeLighting(true);
-		fern.getTexture().setHasTransparency(true);
+		TexturedModel lamp = new TexturedModel(lampModel, new ModelTexture(loader.loadTexture("lamp")));
+		lamp.getTexture().setHasTransparency(true);
 		
 		List<Entity> entities = new ArrayList<Entity>();
-		Random random = new Random(676452);
 		List<Light> lights = new ArrayList<Light>();
-		lights.add(new Light(new Vector3f(0, 10000, -7000), new Vector3f(1, 1, 1)));
-		lights.add(new Light(new Vector3f(-200, 10, -200), new Vector3f(10, 0, 0)));
-		lights.add(new Light(new Vector3f(200, 10, 200), new Vector3f(0, 0, 10)));
+		lights.add(new Light(new Vector3f(0, 1000, -7000), new Vector3f(0.5f, 0.5f, 0.5f)));
+		lights.add(new Light(new Vector3f(185, 10, -185), new Vector3f(2, 0, 0), new Vector3f(1, 0.01f, 0.002f)));
+		lights.add(new Light(new Vector3f(100, 17, -100), new Vector3f(0, 2, 0), new Vector3f(1, 0.01f, 0.002f)));
+		
+		entities.add(new Entity(lamp, new Vector3f(185, -4.7f, -185), 0, 0, 0, 1));
+		entities.add(new Entity(lamp, new Vector3f(100, -4.2f, -100), 0, 0, 0, 1));
+		
 		Terrain terrain = new Terrain(0, -1, loader, texturePack, blendMap, "heightmap");
-		
-		for (int i = 0; i < 100; i++) {
-			float x = random.nextFloat() * 250;
-			float z = random.nextFloat() * -250; 
-			float y = terrain.getHeightOfTerrain(x, z);
-			entities.add(new Entity(grass, new Vector3f(x, y, z), 0, 0, 0, 1));
-			entities.add(new Entity(fern, random.nextInt(4),new Vector3f(x, y, z), 0, random.nextFloat() * 360, 0, 0.6f));
-		}
-		
-		for (int i =0; i < 500; i++) {
-			float x = random.nextFloat() * 250;
-			float z = random.nextFloat() * -250; 
-			float y = terrain.getHeightOfTerrain(x, z);
-			entities.add(new Entity(flower, new Vector3f(x, y, z), 0, random.nextFloat() * 360, 0, random.nextFloat() * 2));
-		}
 		
 		MasterRenderer renderer = new MasterRenderer();
 		
